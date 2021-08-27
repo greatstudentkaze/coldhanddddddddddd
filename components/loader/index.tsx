cc784788df999b3430cd2a6fec84396438160989
greatstudentkaze <dfyz0r@list.ru>
2021-08-27 21:35:50 +0300

    feat(components): add loader component

---
+++ components/loader/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export const Loader = (): JSX.Element => {
+    return (
+        <div className="container">
+            Loading...
+        </div>
+    );
+};

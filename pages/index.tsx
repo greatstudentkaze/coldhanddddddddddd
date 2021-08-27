9607abafb1ece7b97909def65910c35dba3e0bd6
greatstudentkaze <dfyz0r@list.ru>
2021-08-27 23:00:43 +0300

    feat(pages/home): render 'new products' section (SSG)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,38 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 
+import { fetchCategories } from './api/categories';
+import { fetchProducts } from './api/products';
 import { withLayout } from '../layout';
 
-const Home: NextPage = () => {
+import { Section } from '../components/section';
+import { Category, Product } from '../shared/types';
+
+interface Props extends Record<string, unknown> {
+    products: Product[],
+    categories: Category[],
+}
+
+const Home: NextPage<Props> = ({ products, categories }): JSX.Element => {
+    const newProducts = products.filter((product) => product.new);
+
     return (
         <>
             <Head>
                 <title>coldhand</title>
             </Head>
-            <section className="container">
-                <h1>coldhand</h1>
-            </section>
+            <Section className="container" title="Новые поступления" products={newProducts} />
         </>
     );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const categories = await fetchCategories();
+    const products = await fetchProducts();
+
+    return {
+        props: { categories, products }
+    };
 };
 
 export default withLayout(Home);

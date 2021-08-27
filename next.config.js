97c3598774507e858e3c83569ac3661aeaf77350
greatstudentkaze <dfyz0r@list.ru>
2021-08-27 19:49:14 +0300

    feat: add axios and the ability to import svg as a component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            loader: '@svgr/webpack',
+            options: {
+                prettier: false,
+                svgo: true,
+                svgoConfig: {
+                    plugins: [{ removeViewBox: false }],
+                },
+                titleProp: true,
+            },
+            test: /\.svg$/,
+        });
+
+        return config;
+    },
+};

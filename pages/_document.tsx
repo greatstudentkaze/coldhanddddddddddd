d3d6565c35854c9da816b0cc147b420e9ae261f0
greatstudentkaze <dfyz0r@list.ru>
2021-08-27 21:36:54 +0300

    feat(_document.tsx): add Inter and Ruda fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ class AppDocument extends Document {
     render(): JSX.Element {
         return (
             <Html className="page" lang="ru">
-                <Head />
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Ruda:wght@400;700;900&display=swap" rel="stylesheet" />
+                </Head>
                 <body className="page__body">
                     <Main />
                     <NextScript />
